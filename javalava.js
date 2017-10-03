37da07d18e8f6b9f7a6fb3ebcd0592125f5852d3
Eric <erflag96@gmail.com>
2017-10-03 13:34:12 +0200

    commit -t adivinanza

---
+++ javalava.js
@@ -0,0 +1,24 @@
+alert ("¿Crees que lo adivinaras?");
+
+var num1=100;
+var num2=0;
+
+num2 = prompt ('Escribe un numero del 1 al 100' , '');
+
+
+if (num1>num2) { 
+	alert ("Aspira mas alto");
+}
+
+else if (num1<num2) {
+		alert ("Te has pasado");
+}
+
+else if (num1==num2){ 
+		alert ("Has Acertado pequeño trozo de mierda!!!");
+}
+
+else {
+		alert("Coño que pongas numeros joder, ostia puta")
+
+}
